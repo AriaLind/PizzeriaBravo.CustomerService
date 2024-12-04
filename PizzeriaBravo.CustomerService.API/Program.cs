df3c45b808f3c870ffeef626dbd43cae1434c66c
Aria Lind <143350774+AriaLind@users.noreply.github.com>
2024-12-04 14:59:49 +0100

    testtest

--- PizzeriaBravo.CustomerService.API/Program.cs
+++ PizzeriaBravo.CustomerService.API/Program.cs
@@ -9,12 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
-// Add services to the container.
-//builder.Services.AddDbContext<AppDbContext>(
-//    options =>
-//    {
-//        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//    });
 
 builder.Services.AddDbContext<AppDbContext>(
     options =>
